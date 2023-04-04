01520c41ff65acab53669614393c099e40f121e2
achamaro <a.achamaro@gmail.com>
2023-04-05 00:37:55 +0900

    screenshot

---
+++ readme.md
@@ -0,0 +1,29 @@
+# Playwright を使ってスクリーンショット撮る
+
+## Installation
+
+```
+npm i
+npx playwright install
+```
+
+## Configuration
+
+`main.ts`
+
+```typescript
+// ページのURL
+const pageURL = "https://google.com/search?q=playwright";
+// ページロード後の待機時間をミリ秒で指定
+const initialWait = 3000;
+// スクロール後の待機時間をミリ秒で指定
+const scrollWait = 1500;
+// ヘッドレスモードを真/偽で指定
+const headless = false;
+```
+
+## Usage
+
+```
+npx tsx src/main.ts
+```
---
+++ src/main.ts
@@ -0,0 +1,73 @@
+import { chromium } from "playwright";
+import sanitize from "sanitize-filename";
+import sharp from "sharp";
+import { setTimeout } from "timers/promises";
+
+const VW = 1440;
+const VH = 960;
+
+const pageURL = "https://google.com/search?q=playwright";
+const initialWait = 3000;
+const scrollWait = 1500;
+const headless = false;
+
+(async () => {
+  const browser = await chromium.launch({
+    headless,
+  });
+  const page = await browser.newPage();
+  await page.setViewportSize({ width: VW, height: VH });
+  await page.goto(pageURL);
+  await setTimeout(initialWait);
+
+  const title = await page.title();
+
+  const html = await page.locator("html");
+  const height = await html.evaluate((el) => el.scrollHeight);
+  let captures;
+  if (height % VH === 0) {
+    captures = new Array(height / VH).fill(VH);
+  } else {
+    captures = new Array(Math.floor(height / VH)).fill(VH);
+    captures.push(height % VH);
+  }
+
+  const entries = [
+    ...new Array(Math.floor(height / VH)).fill(VH),
+    height % VH,
+  ].filter((h) => h > 0);
+
+  const compositeImages = [];
+  for (const [i, h] of entries.entries()) {
+    if (i !== 0) {
+      await page.mouse.wheel(0, VH);
+      await setTimeout(scrollWait);
+    }
+    compositeImages.push({
+      input: await page.screenshot({
+        clip: {
+          x: 0,
+          y: VH - h,
+          width: VW,
+          height: h,
+        },
+      }),
+      gravity: "northwest",
+      left: 0,
+      top: i * VH,
+    });
+  }
+
+  await browser.close();
+
+  sharp({
+    create: {
+      width: VW,
+      height,
+      channels: 4,
+      background: { r: 255, g: 255, b: 255, alpha: 0 },
+    },
+  })
+    .composite(compositeImages)
+    .toFile(`output/${sanitize(title)}.png`);
+})();
